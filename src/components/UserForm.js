4d41a872a34c32e780186c58566ef07d2aae1d88
whitneyxlachelle <whiggins16@yahoo.com>
2020-01-23 20:19:37 -0600

    mvp met

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 
 import * as Yup from 'yup';
-//import axios from 'axios';
+import axios from 'axios';
 
-const UserForm = ({ values, errors, touched }) => {
+
+const UserForm = ({ values, errors, touched, status }) => {
     const [user, setUser] = useState([]);
+    const [ users ] = useState([]);
+
+    useEffect(() => {
+        status && setUser(users => [...users, status]);
+    }, [status]);
 
     return (
         <div className="user-form">
@@ -13,24 +19,33 @@ const UserForm = ({ values, errors, touched }) => {
 
             <Form>
                 <Field type="text" name="username" placeholder="Username" value={values.username} />
-                {touched.username && errors.username && ( <p className="errors">{errors.username}</p>)}
-                
+                {touched.username && errors.username && (<p className="errors">{errors.username}</p>)}
+
                 <Field type="text" name="email" placeholder="Email" value={values.email} />
-                {touched.email && errors.email && ( <p className="errors">{errors.email}</p>)}
+                {touched.email && errors.email && (<p className="errors">{errors.email}</p>)}
 
                 <Field type="text" name="password" placeholder="Password" value={values.password} />
-                {touched.password && errors.password && ( <p className="errors">{errors.password}</p>)}
+                {touched.password && errors.password && (<p className="errors">{errors.password}</p>)}
 
                 <label className="checkbox">
                     Terms of Service
             <Field type="checkbox" name="terms" />
                     <span className="checkmark" />
                 </label>
+
                 <button type="submit">Submit</button>
             </Form>
+
+            {user.map(users => (
+                <ul key={users.id}>
+                    <li>Username: {users.username}</li>
+                    <li>Email: {users.email}</li>
+                    <li>Password: {users.password}</li>
+                </ul>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 
 // the higher order component 
@@ -53,7 +68,19 @@ const FormikUserForm = withFormik({
         terms: Yup.bool()
     }),
 
-})
-    (UserForm);
+    handleSubmit(values, { setStatus, resetForm }) {
+        console.log("submitting", values);
+        axios
+            .post("https://reqres.in/api/users", values)
+            .then(res => {
+                console.log("it works", res);
+                setStatus(res.data);
+                resetForm();
+            })
+            .catch(error => {
+                console.log(error.res);
+            });
+    }
+})(UserForm);
 
 export default FormikUserForm;
